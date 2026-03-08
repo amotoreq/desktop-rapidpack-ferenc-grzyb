aafd2c534762955df9d98f20c5d10717277af461
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-03-08 18:21:56 +0100

    created new class ParcelCalculator

---
+++ RapidPack/ParcelCalculator.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RapidPack.Classes;
+
+public class ParcelCalculator
+{
+    public int CalculatePrice(int szerokosc, int wysokosc,int glebokosc, int waga, bool expres, int wyborDostawy)
+    {
+        int cena = 10;
+
+        return cena;
+    }
+}
