9184b7629d7417a5582fc7c8efa6cac1a577b348
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-03-09 18:30:29 +0100

    Add output handler

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Controls.Platform;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using RapidPack.Classes;
 
 namespace RapidPack;
 
@@ -8,4 +14,26 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+    
+    ParcelCalculator parcelCalculator = new ParcelCalculator();
+
+    private void CalculatePrice(object? sender, RoutedEventArgs e)
+    {
+        TextBox[] textBoxes = [Wysokosc, Szerokosc, Glebokosc, Waga];
+        TextBlock wycen = Koszt;
+
+        if (textBoxes.All(x => int.TryParse(x.Text, out int result)))
+        {
+            int wysokosc = int.Parse(Wysokosc.Text);
+            int szerokosc = int.Parse(Szerokosc.Text);
+            int glebokosc = int.Parse(Glebokosc.Text);
+            int waga = int.Parse(Waga.Text);
+            bool czyEkspres = Ekspres.IsChecked ?? false;
+            int typPrzesylki = TypPrzesylki.SelectedIndex;
+            
+            int cenaPaczki = parcelCalculator.CalculatePrice(szerokosc, wysokosc, glebokosc, waga, czyEkspres, typPrzesylki);
+            wycen.Text = cenaPaczki.ToString();
+        }
+    }
+
 }
