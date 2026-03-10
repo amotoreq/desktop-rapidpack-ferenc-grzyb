65ce8bb14bcf13f9ca992689e5998efbc425a330
Toast-Jumpscare <pawel.grzyb@cyfroweszkoly.pl>
2026-03-10 16:07:21 +0100

    Update MainWindowTests.cs

--- RapidPack.Tests/MainWindowTests.cs
+++ RapidPack.Tests/MainWindowTests.cs
@@ -1,12 +1,14 @@
 using Avalonia.Controls;
 using Avalonia.Headless.XUnit;
 using Avalonia.Interactivity;
+using RapidPack.Classes;
 
 namespace RapidPack.Tests;
 using Xunit;
 
 public class MainWindowTests
 {
+   
    [AvaloniaFact]
    public void WeightBox_ShouldAlertWhenOver30()
    {
@@ -26,68 +28,34 @@ public class MainWindowTests
      
       Assert.Equal("Paczka za ciężka",Koszt.Text);
    }
+   
+   
    [AvaloniaFact]
    public void Express_WhenChecked_ShouldAdd15ToPrice()
    {
-      var window = new MainWindow();
-      var Szerokosc = window.FindControl<TextBox>("Szerokosc");
-      var Wysokosc = window.FindControl<TextBox>("Wysokosc");
-      var Glebokosc = window.FindControl<TextBox>("Glebokosc");
-      var Waga = window.FindControl<TextBox>("Waga");
-      var Koszt = window.FindControl<TextBlock>("Koszt");
-      var button = window.FindControl<Button>("Wycen");
-      var Ekspres = window.FindControl<CheckBox>("Ekspres");
-      Szerokosc.Text = "20";
-      Wysokosc.Text = "20";
-      Glebokosc.Text = "20";
-      Waga.Text = "0";
-      Ekspres.IsChecked = true;
-      Koszt.Text = $"Paczka o wymiarach 50x60x60 o wadze 0kg \n Z opcją eksresową oraz typem \n Wynosi 25zł";
-      button.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
       
-      Assert.Contains("25",Koszt.Text);
+      var result = ParcelCalculator.CalculatePrice(0,0,0,0,true,0)
+         .ToString();
+      Assert.Contains("25",result);
    }
+   
+   
+   
    [AvaloniaFact]
    public void ParcelOver150cm_ShouldIncreasePrice()
    {
-      var window = new MainWindow();
-      var Szerokosc = window.FindControl<TextBox>("Szerokosc");
-      var Wysokosc = window.FindControl<TextBox>("Wysokosc");
-      var Glebokosc = window.FindControl<TextBox>("Glebokosc");
-      var Waga = window.FindControl<TextBox>("Waga");
-      var Koszt = window.FindControl<TextBlock>("Koszt");
-      var button = window.FindControl<Button>("Wycen");
-        
-      Szerokosc.Text = "50";
-      Wysokosc.Text = "50";
-      Glebokosc.Text = "60";
-      Waga.Text = "0";
-      button.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
-      Koszt.Text = $"Paczka o wymiarach 50x60x60 o wadze 0kg \n Z typem \n Wynosi 11.5zł";
-      Assert.Contains("11.5",Koszt.Text);
+      var result = ParcelCalculator.CalculatePrice(50, 50, 60, 0,false,0)
+         .ToString();
+      Assert.Equal("15",result);
    }
 
+   
    [AvaloniaFact]
    public void ParcelPaleta_ShouldSetPriceTo100()
    {
-      var window = new MainWindow();
-      var Szerokosc = window.FindControl<TextBox>("Szerokosc");
-      var Wysokosc = window.FindControl<TextBox>("Wysokosc");
-      var Glebokosc = window.FindControl<TextBox>("Glebokosc");
-      var Waga = window.FindControl<TextBox>("Waga");
-      var Koszt = window.FindControl<TextBlock>("Koszt");
-      var button = window.FindControl<Button>("Wycen");
-      var TypPrzesylki = window.FindControl<ComboBox>("TypPrzesylki");  
-      Szerokosc.Text = "50";
-      Wysokosc.Text = "50";
-      Glebokosc.Text = "60";
-      Waga.Text = "0";
-      TypPrzesylki.SelectedValue = "Paleta";
-      button.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
-      Koszt.Text = $"Paczka o wymiarach 50x60x60 o wadze 0kg \n Z typem paleta \n Wynosi 100zł";
-      Assert.Contains("100", Koszt.Text);
-      
-      
+      var result = ParcelCalculator.CalculatePrice(1, 1, 1, 1, false, 2)
+         .ToString();
+      Assert.Contains("100", result);
    }
 }
 
